1ab813cdebba67e6d115087a600f4400c29cd406
ymulenll <ymulenll@gmail.com>
2021-12-02 21:52:39 -0300

    web server and devtools config

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
 };
